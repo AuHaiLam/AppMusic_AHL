eeaee4b5cf2d5f94e337b1e60262d412a77fa04a
admin <lamahph07595@fpt.edu.vn>
2019-11-22 12:37:24 +0700

    du an 1

---
+++ app/src/main/java/com/example/AppMusic_AHL/Video_Fragment_ThongTin.java
@@ -0,0 +1,28 @@
+package com.example.AppMusic_AHL;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+public class Video_Fragment_ThongTin extends Fragment {
+
+    ScrollView scrollView;
+
+
+    public Video_Fragment_ThongTin() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_video__fragment__thong_tin, container, false);
+
+
+        return view;
+    }
+}
